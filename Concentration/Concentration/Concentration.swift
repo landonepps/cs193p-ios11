046df1172789bd51e092c45d35dc577a3a12593a
Landon Epps <landon.epps@gmail.com>
2018-03-28 11:58:27 +0900

    Project 1 w/o extra credit

---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,79 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Landon Epps on 3/27/18.
+//  Copyright © 2018 Landon Epps. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    var flipCount = 0
+    var score = 0
+    // if true, the card at the corresponding index in "cards" has been seen
+    var seen = [Bool]()
+    
+    func chooseCard(at index: Int) {
+        // a card was chosen, so increment flipCount
+        flipCount += 1
+        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // one card is face up and another is selected
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    // match found, mark cards as matched
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                    // add 2 to score
+                    score += 2
+                } else {
+                    // no match was found, so deduct points if a card was seen before
+                    if seen[index] == true {
+                        score -= 1
+                    }
+                    if seen[matchIndex] == true {
+                        score -= 1
+                    }
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+                
+                // mark both cards as seen
+                seen[index] = true
+                seen[matchIndex] = true
+            } else {
+                // either no cards or 2 cards are face up
+                // flip all cards down
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                // flip over current selected card and save its index
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+            // add corresponding bools to seen array
+            seen += [false, false]
+        }
+        
+        // MARK: Shuffle the cards
+        var shuffledCards = [Card]()
+        for _ in cards {
+            let randomIndex = Int(arc4random_uniform(UInt32(cards.count)))
+            shuffledCards.append(cards.remove(at: randomIndex))
+        }
+        cards = shuffledCards
+    }
+}
---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Landon Epps on 3/26/18.
+//  Copyright © 2018 Landon Epps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // if we can't use lazy (property observers) make implicitly unwrapped optional
+    // and init in one of the system functions that run after properties are init'd
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet weak var gameScoreLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.index(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+        updateViewFromModel()
+        theme = nil
+    }
+    
+    
+    func updateViewFromModel() {
+        // indices returns countable range of array
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+            }
+        }
+        flipCountLabel.text = "Flips: \(game.flipCount)"
+        gameScoreLabel.text = "Score: \(game.score)"
+    }
+    
+    var emojiChoices = [["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"],
+                        ["🐶", "🐴", "🐧", "🦁", "🦊", "🐠", "🐮", "🐷", "🐵"],
+                        ["🍎", "🍑", "🍒", "🥝", "🍋", "🍉", "🍓", "🍌", "🍊"],
+                        ["🌮", "🌯", "🍱", "🍔", "🌭", "🍕", "🍳", "🥪", "🍝"],
+                        ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🏐", "🏉", "🏸", "🏓"],
+                        ["🇯🇵", "🇺🇸", "🇰🇷", "🇬🇷", "🇨🇳", "🇸🇪", "🇨🇭", "🇳🇵", "🇩🇪"]]
+    
+    var emoji = [Int:String]()
+    var theme: Int?
+
+    func emoji(for card: Card) -> String {
+        
+        if theme == nil {
+            theme = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+        }
+        
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices[theme!].count)))
+            emoji[card.identifier] = emojiChoices[theme!].remove(at: randomIndex)
+        }
+        
+        // we could do if let or even this:
+//        if emoji[card.identifier] != nil {
+//            return emoji[card.identifier]!
+//        } else {
+//            return "?"
+//        }
+        // but this is another way to write it
+        // return optional, but if nil return this other thing
+        return emoji[card.identifier] ?? "?"
+    }
+    
+    
+}
